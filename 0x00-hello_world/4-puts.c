dba70143ed9b82de9cc2ee1280d9ef37beb41c38
Proftoa <ogunjobimoshoodo@gmail.com>
2022-09-09 04:32:13 -0400

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#!/bin/bash
+#include <stdio.h>
+
+/**
+ * *main - Entry point
+ *
+ * Description: "programming is like building a multilingual puzzle"
+ * Return: 0 if exited and non-zero if otherwise
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
--- 0x00-hello_world/README.md
+++ 0x00-hello_world/README.md
@@ -1,1 +1,1 @@
-Write a C program that prints exact
+Write a C program that prints exactly
