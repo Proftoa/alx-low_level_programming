1a2d8fbed7c86323e37acff36e262b7250d63e64
Proftoa <ogunjobimoshoodo@gmail.com>
2022-09-11 01:50:10 -0700

    The last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -6,14 +6,12 @@
  * And whether it is greater than 5, less than 6, or 0
  * Return: Always 0
  */
-
 int main(void)
-
 {
+
 	int n;
 
 	srand(time(0));
-
 	n = rand() - RAND_MAX / 2;
 
 	if ((n % 10) > 5)
