4f521984f316c8ff88626a25dd94f295f6b45154
Proftoa <ogunjobimoshoodo@gmail.com>
2022-09-11 07:05:03 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase, except for q and e
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter ! = 'e' && letter ! = 'q')
+			putchar('\n');
+
+		return (0);
+	}
