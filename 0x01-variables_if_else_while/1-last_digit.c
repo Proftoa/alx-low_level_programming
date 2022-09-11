711aa67b8b1bad609d37f7b2e6071364e4ee16a4
Proftoa <ogunjobimoshoodo@gmail.com>
2022-09-11 05:00:34 -0700

    I sometimes suffer from insomnia

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -9,7 +9,7 @@
  */
 int main(void)
 {
-	int n;
+	int n ;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
@@ -28,7 +28,7 @@ int main(void)
 
 	else
 	{
-		printf("Last digit of %d is %d and is 0\n"
+		printf("Last digit of %d is %d and is 0\n",
 				n, n % 10);
 	}
 
