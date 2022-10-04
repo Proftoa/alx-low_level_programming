996e449ec0ad28ac7b14e491779e45c05e16bcac
Proftoa <ogunjobimoshoodo@gmail.com>
2022-10-04 14:27:30 -0700

    0x0B. C - malloc, free

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -3,7 +3,6 @@
 /**
  * str_concat - function that concatenates two strings.
  *
- *
  *    * @s1: first string
  *
  *     * @s2: second string
@@ -28,21 +27,17 @@ char *str_concat(char *s1, char *s2)
 
 	s2 = "";
 
-
 	for (len1 = 0; s1[len1] != '\0'; len1++)
-
 	;
 
 	for (len2 = 0; s2[len2] != '\0'; len2++)
 	;
-
 	len = len1 + len2;
 	conStr = malloc(sizeof(char) * (len + 1));
 
 	if (conStr == NULL)
 
 	{
-
 		free(conStr);
 		return (NULL);
 
@@ -50,11 +45,9 @@ char *str_concat(char *s1, char *s2)
 
 	for (i = 0; i < len1; i++)
 		conStr[i] = s1[i];
-
 	j = len2;
 
 	for (len2 = 0; len2 <= j; i++, len2++)
 	conStr[i] = s2[len2];
 	return (conStr);
-
-	}
+}
