925c65f236ff36d395af6162a3b5642966ec4882
Proftoa <ogunjobimoshoodo@gmail.com>
2022-10-06 03:30:52 -0700

    To infinity and beyond

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -30,33 +30,26 @@ int main(int argc, char *argv[])
 	return (0);
 
 	}
-
 	for (i = 1; i < argc; i++)
 
 {
-
 	for (j = 0; argv[i][j] != '\0'; j++)
 
 {
-
 	if (!((argv[i][j] >= '0' && argv[i][j] <= '9')
 				|| argv[i][j] == '-'))
 
 {
 	printf("Error\n");
-
 	return (1);
 
 }
 
 																		}
-
 	sum += atoi(argv[i]);
 
 }
-
 	printf("%d\n", sum);
-
 	return (0);
 
 }
