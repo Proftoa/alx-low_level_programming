34b39b600f5cc98e73f0147b507f15d272d28283
Proftoa <ogunjobimoshoodo@gmail.com>
2022-10-10 15:00:49 -0700

    0x0E. C - Structures, typedef

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -5,6 +5,7 @@
 /**
  * new_dog - new element of type struct dog
  *
+ *
  *    * @name: name of new dog
  *
  *     * @age: age of new dog
@@ -32,30 +33,31 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_name = malloc(sizeof(name));
 
 	if (dog_name == NULL)
+
 	{
-		free(new_dog);
-		return (NULL);
+
+	free(new_dog);
+	return (NULL);
 	}
 
 	dog_owner = malloc(sizeof(owner));
 
 	if (dog_owner == NULL)
+
 	{
-		free(dog_name);
-		free(new_dog);
-		return (NULL);
+
+	free(dog_name);
+	free(new_dog);
+	return (NULL);
 	}
 	_strcpy(dog_name, name);
 	_strcpy(dog_owner, owner);
 	new_dog->name = dog_name;
 	new_dog->owner = dog_owner;
 	new_dog->age = age;
-
 	return (new_dog);
 
 }
-
-
 
 /**
  *_strcpy - copies string to given memory location
@@ -64,26 +66,20 @@ dog_t *new_dog(char *name, float age, char *owner)
  *
  *    * @src: where the string is
  *
- *     *
- *
  *      * Return: char
+ *
  */
 
 char *_strcpy(char *dest, char *src)
 
 {
 
-		int len = 0;
+	int len = 0;
 
-		while (*(src + len) != '\0')
-
-{
-
+	while (*(src + len) != '\0')
+	{
 	*(dest + len) = *(src + len);
-
 	len++;
-}
-
 	*(dest + len) = *(src + len);
 
 	return (dest);
