1470910a5d628b95bdea72467cc8ea3717d30de4
Proftoa <ogunjobimoshoodo@gmail.com>
2022-10-27 16:09:01 -0700

    0x14. C - Bit manipulation

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,17 @@
+es (13 sloc)  229 Bytes
+
+#include "main.h"
+
+/**
+ * get_endianness - function that defines if a pc is big or little endianness.
+ * Return: 0 big 1 little
+ */
+int get_endianness(void)
+{
+	int i = 1;
+
+	if ((i >> 31) == 1)
+		return (0);
+	else
+		return (1);
+}
