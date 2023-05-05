6873d6608b0b376f309b06cb2db64d983d2776f6
Proftoa <ogunjobimoshoodo@gmail.com>
2023-05-05 11:45:50 -0400

    0x01-variables_if_else_while Project

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -9,9 +9,11 @@ int main(void)
 {
 	char letter;
 
+/* I use a for loop to iterate over the lowercase letters of the alphabet*/
 	for (letter = 'a'; letter <= 'z'; letter++)
 		putchar(letter);
 
+/*I  call putchar again with the '\n' character to print a new line.*/	
 	putchar('\n');
 	return (0);
 }
--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,12 +9,15 @@ int main(void)
 {
 	char letter;
 
+/* Using a for loop to iterate over the lowercase letters of the alphabet*/
 	for (letter = 'a'; letter <= 'z'; letter++)
 		putchar(letter);
 
+/*Using a for loop to iterate over the uppercase letters of the alphabet*/
 	for (letter = 'A'; letter <= 'Z'; letter++)
 		putchar(letter);
 
+/* Used to print a new line*/
 	putchar('\n');
 
 	return (0);
